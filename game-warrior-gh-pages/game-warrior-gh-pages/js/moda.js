b0d8f82f558e02b604c84a77272b731d4d4a24c7
IceMaDaFaKa <56919704+IceMaDaFaKa@users.noreply.github.com>
2020-01-04 20:25:12 +0200

    ShowMore button js add +2new reviews

--- game-warrior-gh-pages/game-warrior-gh-pages/js/moda.js
+++ game-warrior-gh-pages/game-warrior-gh-pages/js/moda.js
@@ -6,3 +6,23 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+function showMore() {
+    
+  var review = document.getElementsByClassName("more");
+  var btnText = document.getElementById("show-more-btn");
+    if(review[0].style.display == "none"){
+    review[0].style.display = "inline";
+    review[1].style.display = "inline";
+    review[2].style.display = "inline";
+    review[3].style.display = "inline";
+    btnText.innerHTML = "Скрии";
+    }
+    else{
+    review[0].style.display = "none";
+    review[1].style.display = "none";
+    review[2].style.display = "none";
+    review[3].style.display = "none";
+    btnText.innerHTML = "Покажи още"; 
+    }
+}
